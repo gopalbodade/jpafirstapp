498f753995811d35d94e8f1f85f6e4891d713c03
shubhamrathi <shurathi17@gmail.com>
2017-07-04 15:39:50 +0530

    Upload Image

--- src/main/java/com/jpafirstapp/jpafirstapp/controller/StudentController.java
+++ src/main/java/com/jpafirstapp/jpafirstapp/controller/StudentController.java
@@ -5,11 +5,11 @@ import com.jpafirstapp.jpafirstapp.repository.StudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -44,5 +44,17 @@ public List<Student> getList(){
      ResponseEntity<?> entity=ResponseEntity.ok(map);
     return entity;
 }
+    @RequestMapping(value="/uploadimage")
+    public String uploadImage(@RequestParam MultipartFile img)
+    {
+        String imageName=img.getOriginalFilename();
+        File f=new File("/Users/shubhamrathi/Desktop/Image/"+imageName);
+        try {
+            img.transferTo(f);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+     return "uploaded";
+    }
 
 }
